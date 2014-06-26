45ea7cffbafbb39026610cfec4776c006fc091da
David Pedrosa <dpbataller@gmail.com>
2014-06-26 13:53:56 +0200

    Initial Commit

---
+++ DPBVitae/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DPBVitae
+//
+//  Created by David Pedrosa on 26/06/14.
+//  Copyright (c) 2014 David Pedrosa Bataller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DPBVitae/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DPBVitae
+//
+//  Created by David Pedrosa on 26/06/14.
+//  Copyright (c) 2014 David Pedrosa Bataller. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
